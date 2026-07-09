ea129ef1bd3f1d2f9b62616f292f0cf3da48dbf1
kartik77729-gif <kartik77729@gmail.com>
2026-07-09 15:45:59 +0000

    changes

--- app.py
+++ app.py
@@ -4,45 +4,14 @@ from database import engine
 
 app = Flask(__name__)
 
-Jobs = [
-    {
-        "id": 1,
-        "title": "Data Analyst",
-        "loaction": "Bengaluru, India",
-        "salary": "Rs 10,00,000",
-    },
-    {
-        "id": 1,
-        "title": "Data Scientist",
-        "loaction": "Mumbai, India",
-    },
-    {
-        "id": 1,
-        "title": "AI Engineer",
-        "loaction": "Bengaluru, India",
-        "salary": "Rs 14,00,000",
-    },
-    {
-        "id": 1,
-        "title": "Frontend Engineer",
-        "loaction": "Remote",
-        "salary": "Rs 8,00,000",
-    },
-    {
-        "id": 1,
-        "title": "Backend Engineer",
-        "loaction": "Delhi, India",
-        "salary": "Rs 17,00,000",
-    },
-]
-
-def load_jobs_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM jobs"))
-
 @app.route("/")
 def hello_world():
-    return render_template("home.html", jobs=Jobs, company_name="Amazon AWS ")
+    def load_jobs_from_db():
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT * FROM jobs"))
+            jobs = [row._asdict() for row in result.all()]
+            return jobs
+        return render_template("home.html", jobs, company_name="Amazon AWS ")
 
 
 @app.route("/api/jobs")
